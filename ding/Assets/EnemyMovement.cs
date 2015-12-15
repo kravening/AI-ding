530f89d10e7c8c268b3db0c6074d8b2e6b1ed8cb
bennie12345 <bmuijldert@gmail.com>
2015-12-15 10:56:32 +0100

    added playermovement and enemymovement

---
+++ ding/Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _enemyWaypoints;
+    private int currentIndex;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == _enemyWaypoints[currentIndex].transform.position)
+        {
+            currentIndex++;
+            if (currentIndex >= _enemyWaypoints.Count)
+            {
+                currentIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), _enemyWaypoints[currentIndex].transform.position, .1f);
+    }
+}
---
+++ ding/Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    private float _moveSpeed = 5;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(x, y);
+        transform.Translate(movement * _moveSpeed * Time.deltaTime);
+	}
+}
